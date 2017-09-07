63e0cbe98e242068e8f6c7a0f2103a881872a11f
Bably Kumari and snow vilay <snow@>
2017-09-07 13:19:10 -0700

    first test to check user for any input

---
+++ lib/allergies.rb
@@ -0,0 +1,8 @@
+
+
+allergenScore = {1 => "eggs", 2 => "peanuts", 4 => "shellfish", 8 => "strawberries", 16 => "tomatoes", 32 => "chocolate", 64 => "pollen", 128 => "cats"}
+
+puts allergenScore.values
+puts "what are your allergies from the list?"
+
+score = gets.chomp
---
+++ spec/allergies_spec.rb
@@ -0,0 +1,10 @@
+require('allergies')
+require('rspec')
+require('pry')
+
+
+describe("#allergies")do
+  it("checking to just get any value")do
+    expect(("eggs")).to(eq("eggs"))
+  end
+end
